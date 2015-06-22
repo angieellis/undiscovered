9c9e3ef56699bf83ed37d3e2f368dccf3f60032e
spenserhuang <spenserhuang515@gmail.com>
2015-06-21 18:27:30 -0700

    Backbone testing rest

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var ejsLayouts = require("express-ejs-layouts");
 // set up connection to database
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yourguide_development');
@@ -18,10 +19,15 @@ var tours = require('./controllers/tours');
 
 var app = express();
 
+// setting up ejsLayouts
+app.use(ejsLayouts);
+
 // view engine setup
-app.engine('html', require('ejs').renderFile);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.engine('ejs', require('ejs').renderFile);
+app.use(express.static(__dirname + '../public'));
+app.set('views', path.join(__dirname, 'views/'));
+app.set("view engine","ejs");
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
