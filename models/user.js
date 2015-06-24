7b0432396e69984b8ee92217184748578bfb58ce
Apprentice <angiegrace84@gmail.com>
2015-06-23 20:46:54 -0700

    remove required validations from user model

--- models/user.js
+++ models/user.js
@@ -19,13 +19,11 @@ var userSchema = new Schema({
 
   first_name: {
     type: String,
-    required: true,
     trim: true
   },
 
   last_name: {
     type: String,
-    required: true,
     trim: true
   },
 
