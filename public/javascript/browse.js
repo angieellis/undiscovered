94c61a528730aa94a169c28ee85cf1bc5fc9ea11
keioka <koka0828us@gmail.com>
2015-06-23 21:32:52 -0700

    Update getcuurent position and ajax call on browser.js

--- public/javascript/browse.js
+++ public/javascript/browse.js
@@ -1,18 +1,20 @@
 function getCurrentPosition(){
 	var currentPosition;
-	navigator.geolocation.getCurrentPosition(function(position) {
-  	// console.log(position.coords.latitude, position.coords.longitude);
-  	currentPosition = {lat: position.coords.latitude, lng: position.coords.longitude}
-  	// return currentPosition
-	});
-	debugger
+	(function(){
+		navigator.geolocation.getCurrentPosition(function(position) {
+	  	// console.log(position.coords.latitude, position.coords.longitude);
+	  	currentPosition = {lat: position.coords.latitude, lng: position.coords.longitude}
+	  	debugger
+	  	return currentPosition
+		});
+	}());
 	$.ajax({
-		url:"/tours/",
+		url:"",
 		type:"POST",
 		data: currentPosition
-	}).done(function(){
+	}).done(function(response){
 		console.log("success");
-	}).fail(function(){
+	}).fail(function(response){
 		console.log("error");
 	});
 };
